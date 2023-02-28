e6ea0e965288f124223c7fc4f323007e1707e764
ClaytonEduard <clayton_eduard@hotmail.com>
2023-02-28 09:33:43 -0300

    Implementando verificação resultado cara ou coroa

--- app/src/main/java/com/claytoneduard/ResultadoActivity.java
+++ app/src/main/java/com/claytoneduard/ResultadoActivity.java
@@ -3,12 +3,37 @@ package com.claytoneduard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ResultadoActivity extends AppCompatActivity {
+
+    private ImageView imageView ;
+    private Button btnVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        imageView = findViewById(R.id.imageViewResultado);
+        btnVoltar = findViewById(R.id.buttonVoltar);
+
+        //recuperar os dados
+        Bundle dados = getIntent().getExtras();
+        int numero = dados.getInt("numero");
+        if(numero == 0){
+            imageView.setImageResource(R.drawable.moeda_cara);
+        }else{
+            imageView.setImageResource(R.drawable.moeda_coroa);
+        }
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                   finish();
+            }
+        });
     }
 }
